849250df02529586fbf3d545ddef25b43f9238c7
Janaina <jsk22@inf.ufpr.br>
2024-07-30 10:05:59 -0300

    certo

--- heap.s
+++ heap.s
@@ -1,9 +1,8 @@
 .globl original_brk
-.globl topo_pilha
+.globl stack_top
 .section .data 
     original_brk: .quad 0
-    topo_pilha: .quad 0
-    tamanho_do_novo_bloco: .quad 0
+    stack_top: .quad 0
 .section .text
 
 .globl setup_brk
@@ -21,10 +20,11 @@ setup_brk:
     syscall
 
     movq %rax, original_brk
-    movq %rax, topo_pilha
+    movq %rax, stack_top
     popq %rbp
     ret
 
+# Função de unir dois blocos não ocupados - não utilizamos
 merge_block:
     pushq %rbp
     movq %rsp, %rbp
@@ -34,7 +34,7 @@ merge_block:
     # percorre todos os blocos da heap 
     _while_merge:
         cmpq $1, (%r8)
-        je _itera_merge
+        je _iterate_merge
 
         # r10 contém o tamanho do bloco
         movq 8(%r8), %r10
@@ -45,11 +45,11 @@ merge_block:
         # pssando pro próximo bloco
         addq %r10, %r11
 
-        cmpq topo_pilha, %r11
-        je _itera_merge
+        cmpq stack_top, %r11
+        je _iterate_merge
        
         cmpq $1, (%r11)
-        je _itera_merge
+        je _iterate_merge
 
         # adicionando o tamanho do bloco 2 no 1
         addq 8(%r11), %r10
@@ -58,14 +58,14 @@ merge_block:
 
         movq %r10, 8(%r8)
         
-        _itera_merge:
+        _iterate_merge:
         movq 8(%r8), %r9
         addq %r9, %r8
         addq $16, %r8
         
-        cmpq topo_pilha, %r8
-        je _fim_merge
-    _fim_merge:
+        cmpq stack_top, %r8
+        je _end_merge
+    _end_merge:
 
     popq %rbp
     ret
@@ -80,7 +80,7 @@ dismiss_brk:
     syscall
 
     movq %rax, original_brk
-    movq %rax, topo_pilha
+    movq %rax, stack_top
     popq %rbp
     ret
 
@@ -88,21 +88,23 @@ memory_free:
     pushq %rbp
     movq %rsp, %rbp
 
+    # compara se o endereço que está em rdi é menor ao brk original, se for, é inválido
     cmpq original_brk, %rdi
-    jl _erro
+    jl _error
 
     # tentativa de desalocação acima do que fizemos 
-    cmpq topo_pilha, %rdi
-    jge _erro
+    cmpq stack_top, %rdi
+    jge _error
 
+    # zera o bloco, 0 na header
     movq $0, -16(%rdi)
-    jmp _fim_free
+    jmp _end_free
 
-    _erro:
+    _error:
     # retorna 0 em caso de erro
     movq $0, %rax
 
-    _fim_free:
+    _end_free:
 
     popq %rbp
     ret
@@ -114,18 +116,20 @@ open_space:
 
     # salvando o tamanho do bloco
     movq %rdi, %r8
-
+    # passa o tamanho da header
     addq $16, %rdi
-    # atualizando topo da pilha
-    addq topo_pilha, %rdi
+    # atualizando topo da pilha e brk
+    addq stack_top, %rdi
     movq $12, %rax
     syscall
-    movq %rax, topo_pilha
+    movq %rax, stack_top
 
     # retorna o começo do bloco sem o header
     subq %r8, %rax
     
+    # marca o bloco como ocupado
     movq $1, -16(%rax)
+    # adiciona o tamnho na header
     movq %r8, -8(%rax)
 
     popq %rbp
@@ -135,38 +139,42 @@ add_info:
     pushq %rbp
     movq %rsp, %rbp
 
-    # r15 tamanho - tamanho a gente quer
+    # r15 tamanho - tamanho que foi pedido
     movq %r12, %r15
     subq %rdi, %r15
 
+    # se o tamanho que sobrou é menor que 16 não é possível criar novo bloco, então só preenche a header 
     cmpq $16, %r15
-    jl _soh_enfiar
-
-# nova header
+    jl _just_fill
+    
+    # para pular para o próximo bloco (novo alocado)
     # %r10 está com o endereço do bloco com o tamanho escolhido
     movq %r10, %r14
     addq $16, %r14
     addq %rdi, %r14
 
+    # marca novo bloco como desocupado
     movq $0, (%r14)
     # subtrai a nova header
     subq $16, %r15
     movq %r15, 8(%r14)
 
+    # preenche o bloco pedido como ocupado
     movq $1, (%r10)
-    # se deu pra criar numa nova header
     movq %rdi, 8(%r10)
-    jmp _fim_add_info
+    jmp __end_add_info
 
-    _soh_enfiar:
-        # lembrando que em r10 temos o endereço do bloco que achamos
+    # função para preencher a header quando não sobra espaço para outro bloco
+    _just_fill:
+        # lembrando que em %r10 temos o endereço do bloco que achamos
         movq $1, (%r10)
-        # se deu pra criar numa nova header
         movq %r12, 8(%r10)
         
-    _fim_add_info: 
+    __end_add_info: 
 
+    # pula a header
     addq $16, %r10
+    # atualiza brk
     movq $12, %rax
     movq %r10, %rdi
     syscall
@@ -182,58 +190,60 @@ search_block:
 
     movq original_brk, %r13
 
-    cmpq %r13, topo_pilha    
-    je _n_call
+    # se o topo da pilha for igual ao brk original cria o primeiro bloco
+    cmpq %r13, stack_top    
+    je _call_open
 
     movq original_brk, %r8
 
-    # r12 é o tamanho atual
+    # r12 é o tamanho atual - começa com 0
     movq $0, %r12
 
     # percorre todos os blocos da heap 
     _while_search:
-        # primeiro endereço está ocupado??
+        # primeiro endereço está ocupado?
         cmpq $1, (%r8)
-        je _itera_search
+        je _iterate_search
 
         # compara o tamanho dos blocos
         cmpq %r12, 8(%r8) 
-        jl _itera_search
+        jl _iterate_search
         
         # se o tamanho for maior atualiza o valor
         movq 8(%r8), %r12
         # guarda o endereço do bloco em r10
         movq %r8, %r10
 
-        _itera_search:
+        _iterate_search:
         # pega o tamanho do bloco e coloca em %r9
         movq 8(%r8), %r9
         # soma o tamanho do bloco analisado nele mesmo, brk pula para o proximo bloco 
         addq %r9, %r8
         addq $16, %r8
         
-        cmpq topo_pilha, %r8
-        je _fim_search
+        # vê se está no topo da pilha
+        cmpq stack_top, %r8
+        je _end_search
 
         jmp _while_search
-    _fim_search:
+    _end_search:
 
-    # se não tiver bloco alocado livre chama a de abrir
+    # se não tiver bloco alocado livre chama a de abrir novo bloco
     cmpq $0, %r12
-    je _n_call
+    je _call_open
 
+    # se o maior tamanho encontrado for menor que o tamanho pedido cria novo bloco
     cmpq %rdi, %r12
-    jl _n_call
+    jl _call_open
 
+    # se encontrou bloco do tamanho pedido ou maior adiciona as informações
     call add_info
-    jmp _cu
+    jmp _end_search_block
 
-    _n_call:
+    _call_open:
         call open_space
 
-    # enfiar as informações pq a gente tem o tamanho necessário
-
-    _cu:
+    _end_search_block:
 
     popq %rbp
     ret
